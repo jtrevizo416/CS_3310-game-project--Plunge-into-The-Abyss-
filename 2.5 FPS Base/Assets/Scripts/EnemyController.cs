1344b71ce06fee2419369288830bf3f64f54abbe
jtrevizo416 <jtrevizo416@gmail.com>
2020-07-17 23:12:12 -0500

    Add Project

---
+++ 2.5 FPS Base/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int health = 3;//enemy health
+    public GameObject Explosion;//reference to explosion prefab
+
+    public float playerRange = 10f;//range that the player has to be to enter attack mode
+
+    public Rigidbody2D theRB;//rigidbody reference
+
+    public float moveSpeed;//move speed of enemy
+
+    public bool shouldShoot;
+    public float fireRate = .5f;
+    private float shotCounter;
+    public GameObject Bullet;
+    public Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < playerRange) //compare the position of the enemy and the player and see if enemy is close enough
+        {
+            Vector3 playerDirection = PlayerController.instance.transform.position - transform.position;//find which direction the player is facing
+
+            theRB.velocity = playerDirection.normalized * moveSpeed;//set velocity with the maximum of 1 for the player direction value to stop enemy from moving too fast
+
+            if (shouldShoot)
+            {
+                shotCounter -= Time.deltaTime;
+                if (shotCounter <= 0)
+                {
+                    Instantiate(Bullet, firePoint.position, firePoint.rotation);
+                    shotCounter = fireRate;
+                }
+            }
+        }
+        else 
+        {
+            theRB.velocity = Vector2.zero;
+        }
+    }
+
+    public void TakeDamage()
+    {
+        health--;
+        if (health <= 0)
+        {
+            Destroy(gameObject);//if health is 0 or below destroy the enemy object
+            Instantiate(Explosion, transform.position, transform.rotation);//once enemy is killed instantiate explosion animation and face the player
+        }
+    }
+}
---
+++ 2.5 FPS Base/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    public Rigidbody2D theRB;// player rigid body reference
+
+    public float moveSpeed;//movement speed of the player
+
+    private Vector2 moveInput;
+    private Vector2 mouseInput;
+
+    public float mouseSensitivity = 1f; //speed mouse rotates camera
+
+    public Camera viewCam; //Reference to the player camera
+
+    public GameObject bulletImpact;//reference for bullet impact
+    public int currentAmmo;//ammo ammount
+
+    public Animator gunAnim;//reference to the gun animation
+
+    public int currentHealth;//player current health
+    public int maxHealth = 100;//player max health
+    public GameObject deadScreen;//game over screen
+    private bool hasDied;
+
+    public Text healthText, ammoText;//values to display on hud for amount of ammon and health
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()// Start is called before the first frame update
+    {
+        currentHealth = maxHealth;
+        healthText.text = currentHealth.ToString() + "%";//health text
+
+        ammoText.text = currentAmmo.ToString();//Ammo Text
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasDied)//if player is not dead allow movement
+        {
+            //player movement
+            moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));//get input for movement
+
+            Vector3 moveHorizontal = transform.up * -moveInput.x;//Get the up value and multiply it with whatever value we have for horizontal movement
+
+            Vector3 moveVertical = transform.right * moveInput.y;//Get the right/left value and multiply it with whater value we have for the vertical movement
+
+            theRB.velocity = (moveHorizontal + moveVertical) * moveSpeed;
+
+            //player camera controll
+            mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
+
+            viewCam.transform.localRotation = Quaternion.Euler(viewCam.transform.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f));
+
+            //shooting mechanics
+            //GetMouseButton For Full Auto, GetMouseButtonDown for Semi Auto
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (currentAmmo > 0)
+                {
+                    Ray ray = viewCam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));//create raycast for shooting
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))// if something is hit enter loop
+                    {
+                        //Debug.Log("I'm looking at " + hit.transform.name);
+                        Instantiate(bulletImpact, hit.point, transform.rotation);
+
+                        if (hit.transform.tag == "Enemy")
+                        {
+                            hit.transform.parent.GetComponent<EnemyController>().TakeDamage();
+                        }
+                    }
+                    else
+                    {
+
+                        Debug.Log("I'm looking at nothing");
+                    }
+                    currentAmmo--;
+                    gunAnim.SetTrigger("Shoot");
+                    updateAmmoUI();
+                }
+            }
+        }
+    }
+
+    public void TakeDamage(int damageAmount)//player function for taking damage
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)//if player dies show game over screen
+        {
+            deadScreen.SetActive(true);
+            hasDied = true;
+            currentHealth = 0;
+        }
+
+        healthText.text = currentHealth.ToString() + "%";//health text
+    }
+
+    public void AddHealth(int healAmount)//player function for healing
+    {
+        currentHealth += healAmount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        healthText.text = currentHealth.ToString() + "%";
+    }
+
+    public void updateAmmoUI()
+    {
+        ammoText.text = currentAmmo.ToString();//Ammo Text
+    }
+}
