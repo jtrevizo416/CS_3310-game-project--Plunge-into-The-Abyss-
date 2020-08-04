ef65c5b0f43ea4b6ad98ce6012b20179c5f98a9d
jtrevizo416 <jtrevizo416@gmail.com>
2020-08-04 14:24:47 -0500

    Update
    -Imp prefab
    -New Sounds for weapons and background music
    -Fixed weapon animations to prevent spamming

---
+++ 2.5d Fps update/Assets/Scripts/AudioController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController instance;
+
+    public AudioSource ammo, enemyDeath, enemyShot, gunShot, health, playerHurt, pistolShot, shotgunShot, smgShot;//references to various sound effects
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayPistolShot()//play gunshot sound for pistol
+    {
+        pistolShot.Stop();
+        pistolShot.Play();
+    }
+
+    public void PlayShotgunShot()//play gunshot sound for shotgun
+    {
+        shotgunShot.Stop();
+        shotgunShot.Play();
+    }
+
+    public void PlaySmgShot()//play gunshot sound for smg
+    {
+        smgShot.Stop();
+        smgShot.Play();
+    }
+
+    public void PlayAmmoPickup()//Play sound for when player pickups ammo
+    {
+        ammo.Stop();
+        ammo.Play();
+    }
+
+    public void PlayEnemyDeath()//Play sound for when enemy dies
+    {
+        enemyDeath.Stop();
+        enemyDeath.Play();
+    }
+
+    public void PlayEnemyShot()//Play sound for when enemy takes damage
+    {
+        enemyShot.Stop();
+        enemyShot.Play();
+    }
+
+    public void PlayGunshot()//Play sound for when player shoots
+    {
+        gunShot.Stop();
+        gunShot.Play();
+    }
+
+    public void PlayHealthPickup()//Play sound for when player pickups a health pack
+    {
+        health.Stop();
+        health.Play();
+    }
+
+    public void PlayPlayerHurt()//Play sound for when player takes damage
+    {
+        playerHurt.Stop();
+        playerHurt.Play();
+    }
+
+   
+}
---
+++ 2.5d Fps update/Assets/Scripts/PauseMenu1.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu1 : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            UnlockCursor();
+
+            if (isPaused)
+            {
+                LockCursor();
+                ResumeGame();
+
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        Debug.Log("PauseGame function");
+
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+    public void ResumeGame()
+    {
+        Debug.Log("ResumeGame function");
+
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        LockCursor();
+    }
+    public void GoToMainMenu() // Add later when main menu gets created
+    {
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame function");
+
+        Application.Quit(); //"The Application.Quit call is ignored in the Editor." <- from the documentation
+    }
+
+
+    private void LockCursor()//Lock cursor to window and make it dissapear when playing the game
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
