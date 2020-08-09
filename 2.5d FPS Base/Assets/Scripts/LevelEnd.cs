e21e4ae9c89b6527a88f39c18b9479c68f76d1db
AlexxNino <alex.nino1234@gmail.com>
2020-08-09 01:37:28 -0500

    Update Level End
    Added Level ending screen
    -Kill count
    -timer
    
    -buttons-
    --goto main menu
    --quit
    --retry

---
+++ 2.5d FPS Base/Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelEnd : MonoBehaviour
+{
+    public GameObject levelEndMenu;
+    public Text KillsValue,timer;
+    public float finaltime = 0.0f;
+    public static bool isEnded = false;
+
+    void Start()
+    {
+        levelEndMenu.SetActive(false);
+    }
+    private void Update()
+    {
+        
+        if (PlayerController.instance.transform.position.x < -20 && PlayerController.instance.transform.position.x > -33 && PlayerController.instance.transform.position.y > 75 && PlayerController.instance.transform.position.y < 102)
+        {
+            isEnded = true;
+            KillsValue.GetComponent<UnityEngine.UI.Text>().text = EnemyController.enemykills.ToString();
+            timer.GetComponent<UnityEngine.UI.Text>().text = PlayerController.time.ToString("F2");
+            UnlockCursor();
+            levelEndMenu.SetActive(true);
+            
+        }
+        
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame function");
+
+        Application.Quit(); //"The Application.Quit call is ignored in the Editor." <- from the documentation
+    }
+    public void RetryLevel()
+    {
+        LockCursor();
+        Application.LoadLevel(Application.loadedLevel);
+        EnemyController.enemykills = 0;
+    }
+    public void GoToMainMenu()
+    {
+        LockCursor();
+        Application.LoadLevel(0);
+        
+    }
+    private void LockCursor()//Lock cursor to window and make it dissapear when playing the game
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+ 
+
+}
---
+++ 2.5d FPS Base/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    public static float time = 0;
+
+    public Rigidbody2D theRB;// player rigid body reference
+
+    public float moveSpeed;//movement speed of the player
+
+    private Vector2 moveInput;
+    private Vector2 mouseInput;
+
+    public float mouseSensitivity = 1f; //speed mouse rotates camera
+
+    public Camera viewCam; //Reference to the player camera
+
+   // public GameObject bulletImpact;//reference for bullet impact
+   // public int currentAmmo;//ammo ammount
+
+   // public Animator gunAnim;//reference to the gun animation
+    public Animator anim;//reference to headbob animation
+
+    public int currentHealth;//player current health
+    public int maxHealth = 100;//player max health
+    public GameObject deadScreen;//game over screen
+    private bool hasDied;
+
+    public Text healthText; //ammoText;//values to display on hud for amount of ammon and health
+
+    public bool headbob;//enable or disable headbob
+
+    public HealthBar healthBar; // Health bar slider class
+    public HP_TEXT hptext;//TEXT HP
+
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()// Start is called before the first frame update
+    {
+        currentHealth = maxHealth;
+        //healthText.text = currentHealth.ToString() + "%";//health text
+
+        //ammoText.text = currentAmmo.ToString();//Ammo Text
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (LevelEnd.isEnded == false)
+        {
+            time += Time.deltaTime;
+        }
+
+        if (!hasDied)//if player is not dead allow movement
+        {
+            //player movement
+            moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));//get input for movement
+
+            Vector3 moveHorizontal = transform.up * -moveInput.x;//Get the up value and multiply it with whatever value we have for horizontal movement
+
+            Vector3 moveVertical = transform.right * moveInput.y;//Get the right/left value and multiply it with whater value we have for the vertical movement
+
+            theRB.velocity = (moveHorizontal + moveVertical) * moveSpeed;
+
+            //player camera controll
+            mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
+
+            viewCam.transform.localRotation = Quaternion.Euler(viewCam.transform.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f));
+
+            if (headbob)//if enabled
+            {
+                if (moveInput != Vector2.zero)//if player is moving headbob
+                {
+                    anim.SetBool("IsMoving", true);
+                }
+                else
+                {
+                    anim.SetBool("IsMoving", false);
+                }
+            }
+            else if (!headbob)//if disabled
+            {
+                anim.SetBool("IsMoving", false);
+            }
+        }
+    }
+
+    public void TakeDamage(int damageAmount)//player function for taking damage
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)//if player dies show game over screen
+        {
+            deadScreen.SetActive(true);
+            hasDied = true;
+            currentHealth = 0;
+        }
+
+        healthBar.SetHealth(currentHealth);
+        hptext.SetHealthHP(currentHealth);
+
+        AudioController.instance.PlayPlayerHurt();//When player takes damage play player hurt sound effect
+    }
+
+    public void AddHealth(int healAmount)//player function for healing
+    {
+        {
+            currentHealth += healAmount;
+            if (currentHealth > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+
+            healthBar.SetHealth(currentHealth);
+            hptext.SetHealthHP(currentHealth);
+        }
+    }
+
+    //public void updateAmmoUI()
+    //{
+    //    ammoText.text = currentAmmo.ToString();//Ammo Text
+    //}
+}
